46a1cf9b104997e7cf257d9f6cea8e9bdf07ee62
Danyil Deli <danyil.deli@globallogic.com>
2019-09-26 13:46:30 +0300

    Test functionality of bot memory

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const options = {
   },
 };
 
+const userInfo = {
+} ;
+
+const rememberMode = {}
+
 const bot = new TelegramBot(token, options);
 bot.setWebHook(`${url}/bot${token}`);
 
@@ -16,3 +21,27 @@ bot.on('message', (msg) => {
   const response = msg.text === '/start' ? `Привет, ${msg.from.first_name}! Отправь мне любое сообщение и я перепишу его в обратном порядке.` : msg.text.split('').reverse().join('');
   bot.sendMessage(chatId, response);
 });
+
+bot.onText(/\/remember/, (msg) => {
+  const chatId = msg.chat.id;
+  let text = '';
+  if ((`${msg.from.id}` in userInfo)) {
+    text = userInfo[`${msg.from.id}`].hobby;
+  } else {
+    text = 'I do not know your hobby';
+    rememberMode[`${msg.from.id}`] = {}
+  }
+  bot.sendMessage(chatId, text);
+  console.log(msg);
+})
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  if ((`${msg.from.id}` in rememberMode)) {
+    userInfo[`${msg.from.id}`] = {
+      hobby: msg.text
+    }
+    delete rememberMode[`${msg.from.id}`];
+    bot.sendMessage(chatId, 'Hobby added');
+  }
+})
